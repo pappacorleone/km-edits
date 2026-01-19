95072f41ecf3c01f2d5dbf169963bca5f36d6cde
yannisdia <y.diarra@workadventu.re>
2026-01-19 09:58:28 +0100

    fix ci

--- play/src/front/Components/ActionBar/MenuIcons/ContextualMenuItems.svelte
+++ play/src/front/Components/ActionBar/MenuIcons/ContextualMenuItems.svelte
@@ -52,7 +52,6 @@
     <FollowMenuItem />
 {/if}
 
-
 {#if $currentPlayerAreaIdStore !== undefined || ($bottomActionBarVisibilityStore && !$inLivekitStore && $currentPlayerGroupLockStateStore !== undefined)}
     <LockDiscussionMenuItem />
 {/if}
